6f3c65d6ff339ce5d5f85f728c4ebccd7a05762e
Takeo Asai <spitson+github@gmail.com>
2016-03-13 20:43:35 +0900

    bind model-controller

---
+++ example/src/models/todo.js
@@ -0,0 +1,19 @@
+angular.module('todo.services', [])
+
+.factory('Todos', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var todos = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get higeeeh enough to grab the flag' },
+    { title: '日本語 the Princess' }
+  ];
+
+  return {
+    all: function() {
+      return todos;
+    }
+  };
+});
