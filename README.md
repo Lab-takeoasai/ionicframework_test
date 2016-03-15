6665c8f461ce778323c1b3be256e571d0b29a1ca
Takeo Asai <spitson+github@gmail.com>
2016-03-16 03:59:05 +0900

    test added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ionicframework_test
+
+npm install -g karma-cli
---
+++ example/test/sampleTest.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('this test should be', () => {
+  it('always passed', () => {
+    expect(1).toEqual(1);
+  });
+});
